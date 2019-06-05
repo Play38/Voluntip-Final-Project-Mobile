14703c3277cd9a5fe449107c6fe4a3a8517c946d
Ofir Cohen <37571916+Play38@users.noreply.github.com>
2019-06-05 12:13:34 +0300

    Map with custom markers

--- src/Screens/MainPage.js
+++ src/Screens/MainPage.js
@@ -4,7 +4,36 @@ import { PropTypes } from 'prop-types'
 import styles from '../Comp/Styles/containerStyle'
 import { BottomNavigation } from 'react-native-paper';
 import RedeemPage from './RedeemPage'
+import MapView, { Marker } from 'react-native-maps';
+
+
+
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 export default class MyComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      markers: []
+    }
+    this.handlePress = this.handlePress.bind(this);
+  }
+  handlePress(e) {
+    this.setState({
+      markers: [
+        ...this.state.markers,
+        {
+          coordinate: e.nativeEvent.coordinate,
+          cost: `Voluntip coins ${getRandomInt(20, 100)}`
+        }
+      ]
+    })
+  }
     static propTypes = {
         navigation: PropTypes.any.isRequired
     }
@@ -39,14 +68,44 @@ export default class MyComponent extends Component {
     code: this.CodeRoute,
   });
 
+
   render() {
     return (
-      <BottomNavigation
-        navigationState={this.state}
-        onIndexChange={this._handleIndexChange}
-        renderScene={this._renderScene}
-        barStyle = {{ backgroundColor: '#87ceeb' }}
-      />
-    )
+        <MapView
+            style={stylesMap.container}
+            initialRegion={{
+              latitude: 32.090034,
+              latitudeDelta: 0.01,
+              longitude: 34.803028,
+              longitudeDelta: 0
+            }}
+            onPress={this.handlePress}
+        >
+          {this.state.markers.map((marker) => {
+            return (
+                <Marker {...marker} >
+                  <View style={stylesMap.marker}>
+                    <Text style={stylesMap.text}>{marker.cost}</Text>
+                  </View>
+                </Marker>
+            )
+          })}
+        </MapView>
+    );
   }
 }
+
+const stylesMap = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  marker: {
+    backgroundColor: "#550bbc",
+    padding: 5,
+    borderRadius: 5,
+  },
+  text: {
+    color: "#FFF",
+    fontWeight: "bold"
+  }
+});
