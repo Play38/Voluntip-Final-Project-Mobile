ae2f68d977a513cbe852f6a834935006585ff669
Ofir Cohen <37571916+Play38@users.noreply.github.com>
2019-06-11 19:37:28 +0300

    fix

--- src/Screens/Store.js
+++ src/Screens/Store.js
@@ -37,7 +37,7 @@ export default class Store extends Component {
     id = {this.state.id}
     coins = {this.state.coins}
     price = {100}
-    name = {"Science camp "}
+    name = {"AAAAAAA"}
     img = {'https://facebook.github.io/react-native/docs/assets/favicon.png'}
     update = {this.update}
     />
@@ -45,7 +45,7 @@ export default class Store extends Component {
     id = {this.state.id}
     coins = {this.state.coins}
     price = {200}
-    name = {"Albums"}
+    name = {"BBBBBBB"}
     img = {'https://scontent.ftlv6-1.fna.fbcdn.net/v/t1.0-9/62100067_1702640313171655_5939079651627368448_o.jpg?_nc_cat=108&_nc_ht=scontent.ftlv6-1.fna&oh=dcf7721c85d0a96fde8e8598df0bc5bc&oe=5D9A3D29'}
     update = {this.update}
     />
